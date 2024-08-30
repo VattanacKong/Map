88f91f9221c055aeafca3f6965a9f14cc0a18700
VattanacKong <65273818+VattanacKong@users.noreply.github.com>
2024-08-30 14:00:28 +0700

    Add files via upload

---
+++ BFS.py
@@ -0,0 +1,38 @@
+import json
+
+with open('/mnt/d/Tux/Math for AI/map project/Data/cleaned3.json', 'r') as file:
+    districts = json.load(file)
+
+
+def get_path(destination, origin, store,path = []):   
+    for i in store:
+        if i[0] == destination:
+            path.append(i)
+            break
+    if path[-1][1] == origin:
+        return path
+    else:
+        new_destination = path[-1][1]
+        return get_path(new_destination, origin, store, path)
+
+
+def bfs(origin, destination, districts, open =[], close= [], store = []): 
+    for neighbor in districts[origin]:
+        if (neighbor not in open) and (type(neighbor) is str) and(neighbor not in close):
+            open.append(neighbor)
+            store.append([neighbor,origin])
+    if origin not in close:
+        close.append(origin)
+    if open[0] == destination:
+        path = get_path(destination, close[0], store)
+        real_path = [destination]
+        for i in path:
+            real_path.insert(0,i[1])
+        print(real_path)
+        return real_path
+    else:
+        next_start = open[0]
+        open.remove(next_start)
+        return bfs(next_start,destination,districts, open, close, store)
+            
+bfs('Tuol Kouk', "Kandal Stueng", districts)
